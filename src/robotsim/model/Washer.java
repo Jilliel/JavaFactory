22928011f214adbd43d1c8507469176940c2929f
Raphael Dalle Risbourg <raphael.dalle@telecom-paris.fr>
2025-05-08 18:42:23 +0200

    Ajout du radius en attribut

--- src/robotsim/model/Washer.java
+++ src/robotsim/model/Washer.java
@@ -4,10 +4,20 @@ import shapes.Oval;
 public class Washer extends Component{
 	
 	private double weight;
-
+	private int radius;
+	
 	public Washer(int x, int y, int radius, double weight, String name) {
 		super(x, y, new Oval(radius, radius), name);
 		this.weight = weight;
+		this.radius = radius;
+	}
+	
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
 	}
 
 	public double getWeight() {
