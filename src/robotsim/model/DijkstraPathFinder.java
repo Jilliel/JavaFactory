453c2e293acb44392c92786811e56a01f520be26
Raphael Dalle Risbourg <raphael.dalle@telecom-paris.fr>
2025-05-10 15:58:24 +0200

    Ajout d'une classe pour le path finder

---
+++ src/robotsim/model/DijkstraPathFinder.java
@@ -0,0 +1,17 @@
+package robotsim.model;
+import java.util.List;
+import java.io.Serializable;
+
+public class DijkstraPathFinder implements FactoryPathFinder, Serializable{
+	
+	private static final long serialVersionUID = 202505090931L;
+	
+	public DijkstraPathFinder() {
+		super();
+	}
+	
+	public List<Position> findPath(Position start, Position end) {
+		return null;
+	}
+	
+}
