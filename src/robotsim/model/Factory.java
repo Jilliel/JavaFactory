9837d6cbd2fc5e08dbebb89f13719cf0f49457d5
Raphael Dalle Risbourg <raphael.dalle@telecom-paris.fr>
2025-05-10 19:25:22 +0200

    Ajout d'une fonction pour vérifier la validité d'une position

--- src/robotsim/model/Factory.java
+++ src/robotsim/model/Factory.java
@@ -2,11 +2,14 @@ package robotsim.model;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.lang.Math;
+
 import shapes.Rectangle;
 import fr.tp.inf112.projects.canvas.model.Canvas;
 import fr.tp.inf112.projects.canvas.model.Figure;
 import fr.tp.inf112.projects.canvas.model.Shape;
 import fr.tp.inf112.projects.canvas.model.Style;
+import robotsim.SimulatorApplication;
 import fr.tp.inf112.projects.canvas.controller.Observable;
 import fr.tp.inf112.projects.canvas.controller.Observer;
 
@@ -15,6 +18,9 @@ import java.io.Serializable;
 public class Factory extends Component implements Canvas, Serializable, Observable {
 	
 	private static final long serialVersionUID = 202505090919L;
+	public static final int spatialStep = 3;
+	
+	private String id;
 	
 	private int width;
 	private int height;
@@ -23,25 +29,21 @@ public class Factory extends Component implements Canvas, Serializable, Observab
 	private List<Robot> robots;
 	
 	private transient List<Observer> observers;
-	private boolean simulationRunning;
-	
-	private String id;
-	
+	private boolean simulationRunning;	
 	
 	public Factory(String name, Position position, int width, int height) {
 		super(position, name, null);
-		this.robots = new ArrayList<Robot>();
-		this.rooms = new ArrayList<Room>();
 		this.width = width;
 		this.height = height;
+		this.rooms = new ArrayList<Room>();
+		this.robots = new ArrayList<Robot>();
 		this.observers = new ArrayList<Observer>();
 		this.simulationRunning = false;
-		
 	}
 	
 	public boolean canProduce(ProductionMachine machine) {
 		int targetX = machine.getxCoordinate() + (machine.getWidth()/2) - ProductionMachine.getWasherRadius();
-		int targetY = machine.getyCoordinate() + (machine.getHeight()/2)- ProductionMachine.getWasherRadius();
+		int targetY = machine.getyCoordinate() + (machine.getHeight()/2) - ProductionMachine.getWasherRadius();
 		for (Room room : this.rooms) {
 			for (ProductionArea area : room.getAreas()) {
 				for (Washer washer : area.getMaterials()) {
@@ -116,6 +118,43 @@ public class Factory extends Component implements Canvas, Serializable, Observab
 		} else {
 			return false;
 		}
+	}
+	
+	public boolean getValidity(Position position) {
+		List<Component> obstacles = this.getObstacles();
+		int x = position.getX();
+		int y = position.getY();
+		if (x < 0 || x > this.getWidth() || y < 0 || y > this.getHeight()) {
+			return false;
+		}
+
+		for (Component obstacle : obstacles) {
+			if (obstacle instanceof Door) {
+				Door door = (Door) obstacle;
+				if (door.isOpen()) {
+					continue;
+				} else if (Math.abs(door.getxCoordinate() + door.getWidth() / 2 - x) > SimulatorApplication.robotRadius + door.getWidth() / 2) {
+					continue;
+				} else if (Math.abs(door.getyCoordinate() + door.getHeight() / 2 - y) > SimulatorApplication.robotRadius + door.getHeight() / 2) {
+					continue;
+				} else {
+					return false;
+				}
+			} else if (obstacle instanceof Room) {
+				Room room = (Room) obstacle;
+				int leftwall = room.getxCoordinate();
+				int rightwall = room.getxCoordinate() + room.getHeight();
+				if (Math.abs(x - leftwall) < SimulatorApplication.robotRadius || Math.abs(x - rightwall) < SimulatorApplication.robotRadius) {
+					return false;
+				}
+				int upperwall = room.getyCoordinate();
+				int downwall = room.getyCoordinate() + room.getHeight();
+				if (Math.abs(y - upperwall) < SimulatorApplication.robotRadius || Math.abs(y - downwall) < SimulatorApplication.robotRadius) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 	
 	public List<Component> getObstacles() {
