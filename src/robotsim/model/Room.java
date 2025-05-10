645630db6b6bccbb965b4bf8caa5de26024b6451
Raphael Dalle Risbourg <raphael.dalle@telecom-paris.fr>
2025-05-10 19:24:56 +0200

    Ajout de getter et setter

--- src/robotsim/model/Room.java
+++ src/robotsim/model/Room.java
@@ -27,6 +27,22 @@ public class Room extends Component implements Serializable{
 		this.stations = new ArrayList<ChargingStation>();
 	}
 	
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 	public List<Door> getDoors() {
 		return doors;
 	}
